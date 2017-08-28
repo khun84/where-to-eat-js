aefc8ca690c8065044ee653748fb0f28bb6693fc
khun84 <khun84@gmail.com>
2017-08-28 11:21:12 +0800

    code completion

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-function addRestaurantListener(){
-  startBtn = document.getElementById('start-random-button');
-  
-  // Replace 'functionName' with the name of the function that you have written
-  startBtn.addEventListener("click", functionName)
+function addRestaurantListener() {
+    startBtn = document.getElementById('start-random-button');
 
-  getBtn = document.getElementById('get-restaurant-button');
-  
-  // Replace 'functionName' with the name of the function that you have written
-  getBtn.addEventListener("click", functionName);
+    // Replace 'functionName' with the name of the function that you have written
+    startBtn.addEventListener("click", startRandom)
+
+    getBtn = document.getElementById('get-restaurant-button');
+
+    // Replace 'functionName' with the name of the function that you have written
+    getBtn.addEventListener("click", selectRestaurant);
+}
+
+restaurants = ['Nasi Goreng', 'McDonald', 'Kentucky', 'Mixed Rice', 'Pork Noodle'];
+
+// var getFlag = false;
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var nIntervalID;
+
+function startRandom() {
+    nIntervalID = setInterval(showRestaurant, 100);
+}
+
+function showRestaurant() {
+    var paraRestaurant = document.getElementById('restaurant');
+    paraRestaurant.innerHTML = getRestaurant();
+    
+}
+
+function getRestaurant() {
+    var randInt = getRandomInt(0, restaurants.length);
+    return restaurants[randInt];
+}
+
+function selectRestaurant() {
+    clearInterval(nIntervalID);
 }
 
 // wait for DOM content to load before binding an event
