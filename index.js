f8f5dd3c3333da7559e8e669444d25f33d4b4e06
khun84 <khun84@gmail.com>
2017-08-28 12:03:52 +0800

    added color blink and result message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function addRestaurantListener() {
     startBtn = document.getElementById('start-random-button');
 
     // Replace 'functionName' with the name of the function that you have written
-    startBtn.addEventListener("click", startRandom)
+    startBtn.addEventListener("click", startRandom);
 
     getBtn = document.getElementById('get-restaurant-button');
 
@@ -11,7 +11,11 @@ function addRestaurantListener() {
 }
 
 restaurants = ['Nasi Goreng', 'McDonald', 'Kentucky', 'Mixed Rice', 'Pork Noodle'];
+var colors = [];
 
+for (var i=0;i<restaurants.length;i++){
+    colors.push(getRandomColor())
+}
 // var getFlag = false;
 
 function getRandomInt(min, max) {
@@ -28,17 +32,37 @@ function startRandom() {
 
 function showRestaurant() {
     var paraRestaurant = document.getElementById('restaurant');
-    paraRestaurant.innerHTML = getRestaurant();
-    
-}
+    var index = getRandomInt(0, restaurants.length);
+    paraRestaurant.innerHTML = restaurants[index];
+    paraRestaurant.style.color = colors[index];
 
-function getRestaurant() {
-    var randInt = getRandomInt(0, restaurants.length);
-    return restaurants[randInt];
+    //
+    if (document.getElementById('result-message')){
+        var resultMessage = document.getElementById('result-message');
+        resultMessage.innerHTML='';
+    }
+
 }
 
 function selectRestaurant() {
     clearInterval(nIntervalID);
+
+    var paraStatement = document.createElement('p');
+    paraStatement.setAttribute('id','result-message');
+    var restaurant = document.getElementById('restaurant').textContent;
+    paraStatement.innerHTML = "Great, let's go to " + restaurant + '!';
+    paraStatement.style.textAlign = 'center';
+    var parentNode = document.getElementById('get-restaurant-button').parentNode;
+    parentNode.insertBefore(paraStatement,document.getElementById('get-restaurant-button').nextSibling);
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 // wait for DOM content to load before binding an event
